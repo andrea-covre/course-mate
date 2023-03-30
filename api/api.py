4eabada109eb832b0f0c3cab7da2cfc87a289c94
Pranav <pthomas76@gatech.edu>
2023-03-30 12:41:47 -0400

    Add more models

--- api/api.py
+++ api/api.py
@@ -2,10 +2,8 @@ import json
 from flask import Flask, request, jsonify
 from flask_restful import Api
 from planetscale_connection import get_db_session
-from models import major
-from models import account
+from models import major, account, schedule, section, semester, class_tbl, location, instructor
 from sqlalchemy import select
-from sqlalchemy.orm import Session
 
 session = get_db_session()
 app = Flask(__name__)
@@ -51,6 +49,62 @@ def update_account():
         
     session.commit()
     return feature.as_dict(), 200
+
+#TO BE TESTED
+#Example = /users/schedule?id=<user_id>
+@app.route('/users/schedule', methods=['GET'])
+def update_account():
+    data = request.get_json(force=True)
+    data = json.loads(data)
+    id = data['account_id']
+    stmt = select(schedule.Schedule).where(
+        schedule.Schedule.account_id.in_([id])
+    )
+    schedule_objects = []
+    for obj in session.scalars(stmt):
+        schedule_objects.append(obj)
+
+    sections = []
+    for schedule_object in schedule_objects:
+        section_id = schedule_object.section_id
+        stmt = select(section.Section).where(
+            section.Section.id.in_([section_id])
+        )
+        curr_section = session.scalar(stmt)
+        sections.append(curr_section)
+
+    schedule = []
+    for curr_section in sections:
+        curr_schedule = {}
+        semester_id = curr_section.semester_id
+        curr_semester = session.scalar(select(semester.Semester).where(
+            semester.Semester.id.in_([semester_id])
+        ))
+        curr_schedule['semester'] = curr_semester.as_dict()
+
+        class_id = curr_section.class_id
+        curr_class = session.scalar(select(class_tbl.Class).where(
+            class_tbl.Class.id.in_([class_id])
+        ))
+        curr_schedule['class'] = curr_class.as_dict()
+        curr_schedule['crn'] = curr_section.crn
+        curr_schedule['section_name'] = curr_section.section_name
+        instructor_id = curr_section.instructor_id
+        curr_instructor = session.scalar(select(instructor.Instructor).where(
+            instructor.Instructor.id.in_([instructor_id])
+        ))
+        curr_schedule['instructor'] = curr_instructor.as_dict()
+        curr_schedule['times'] = curr_section.times
+
+        location_id = curr_section.location_id
+        curr_location = session.scalar(select(location.Location).where(
+            location.Location.id.in_([location_id])
+        ))
+        curr_schedule['semester'] = curr_location.as_dict()
+        schedule.append(curr_schedule)
+
+    return schedule, 200
     
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
+
