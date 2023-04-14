8041e7e0064e761c90e0e8640e2d41d5828259ff
andrea-covre <85234549+andrea-covre@users.noreply.github.com>
2023-04-14 14:44:59 -0400

    added unique constraints

--- api/models/account.py
+++ api/models/account.py
@@ -1,6 +1,6 @@
 from typing import List
 from typing import Optional
-from sqlalchemy import String
+from sqlalchemy import String, UniqueConstraint
 from sqlalchemy.orm import Mapped, mapped_column
 
 from api.models.base import Base
@@ -9,13 +9,17 @@ class Account(Base):
     __tablename__ = "account"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    email_address: Mapped[str] = mapped_column(String(320))
-    edu_email_address: Mapped[str] = mapped_column(String(320))
+    email_address: Mapped[str] = mapped_column(String(320), unique=True)
+    edu_email_address: Mapped[str] = mapped_column(String(320), unique=True)
     first_name: Mapped[str] = mapped_column(String(50))
     last_name: Mapped[str] = mapped_column(String(50))
-    phone_number: Mapped[str] = mapped_column(String(15))
+    phone_number: Mapped[str] = mapped_column(String(15), unique=True)
     grad_year: Mapped[int]
     major_id: Mapped[int]
+    
+    __table_args__ = (UniqueConstraint('email_address', name='uq_email_address'),)
+    __table_args__ = (UniqueConstraint('edu_email_address', name='uq_edu_email_address'),)
+    __table_args__ = (UniqueConstraint('phone_number', name='phone_number'),)
 
     def as_dict(self):
         return {c.name: getattr(self, c.name) for c in self.__table__.columns}
